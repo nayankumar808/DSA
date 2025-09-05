5985379815aebb171fdadc6377ae694c482efdbd
nayan kumar <nayans808@gmail.com>
2025-09-05 23:28:01 +0530

    m

--- Basic_maths/count_digit.java
+++ Basic_maths/count_digit.java
@@ -1,5 +1,21 @@
 package Basic_maths;
 
 public class count_digit {
+  public static int evenlyDivides(int n) {
+    int m = n;
+    int count = 0;
+    while (m > 0) {
+      int ld = m % 10;
+      if (ld != 0 && n % ld == 0) {
+        count++;
+      }
+      m = m / 10;
+    }
+    return count;
+  }
 
+  public static void main(String[] args) {
+    int n = 1012;
+    System.out.println("Digits that evenly divide " + n + " = " + evenlyDivides(n));
+  }
 }
