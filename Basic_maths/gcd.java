5d6056e9617dcf73bbd4445e8fe987cd2fd1091c
nayan kumar <nayans808@gmail.com>
2025-09-24 08:22:10 +0530

    _

---
+++ Basic_maths/gcd.java
@@ -0,0 +1,19 @@
+package Basic_maths;
+
+public class gcd {
+
+  public static int Gcd(int x) {
+    int gcd = 0;
+    while (x != 0) {
+
+    }
+    return gcd;
+  }
+
+  public static void main(String[] args) {
+    int num = 123;
+    System.out.println(Gcd(num));
+
+  }
+
+}
