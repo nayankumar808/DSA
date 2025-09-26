f0ad82731246a9739b5febadd42189118082cad3
nayan kumar <nayans808@gmail.com>
2025-09-26 07:32:33 +0530

    _

--- Basic_maths/gcd.java
+++ Basic_maths/gcd.java
@@ -2,17 +2,30 @@ package Basic_maths;
 
 public class gcd {
 
-  public static int Gcd(int x) {
-    int gcd = 0;
-    while (x != 0) {
+  public static int Gcd(int x, int y) {
+    if (x == 0 || y == 0) {
+      return 0;
+
+    }
+    if (x == 0)
+      return y;
+    if (y == 0)
+      return x;
+    int gcd = 1;
+
+    for (int i = 1; i <= Math.min(x, y); i++) {
+
+      if (x % i == 0 && y % i == 0) {
+        gcd = i;
+      }
 
     }
     return gcd;
+
   }
 
   public static void main(String[] args) {
-    int num = 123;
-    System.out.println(Gcd(num));
+    System.out.println(Gcd(7, 14));
 
   }
 
