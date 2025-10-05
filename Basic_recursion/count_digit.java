bf0bcd5e81017418b0a7e999ba753009d67788d9
nayan kumar <nayans808@gmail.com>
2025-10-05 23:31:12 +0530

    _

---
+++ Basic_recursion/count_digit.java
@@ -0,0 +1,18 @@
+package Basic_recursion;
+
+public class count_digit {
+  int count = 0;
+
+  public void print(int n) {
+    if (count == n) {
+      return;
+    }
+    System.out.println("nayan");
+    count++;
+
+  }
+
+  public void main(String[] args) {
+    print(5);
+  }
+}
