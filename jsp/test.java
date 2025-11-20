235c18717d1177ba64196bfb84f9a8a9008f6b69
nayan kumar <nayans808@gmail.com>
2025-11-20 23:00:49 +0530

    _

---
+++ jsp/test.java
@@ -0,0 +1,74 @@
+package jsp;
+
+import java.util.Scanner;
+
+public class test {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter the value of n");
+    int n = scn.nextInt();
+    // System.out.println(primeNumbers(n));
+    // System.out.println(palindrome(n));
+    // System.out.println(perfect(n));
+    System.out.println(strong(n));
+  }
+
+  public static boolean primeNumbers(int n) {
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+      if (n % i == 0)
+        count++;
+    }
+    if (count == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static boolean palindrome(int n) {
+    int num = n;
+    int digit = 0;
+    int reverse = 0;
+
+    while (num != 0) {
+      digit = num % 10;
+      reverse = (reverse * 10) + digit;
+      num = num / 10;
+
+    }
+    return (reverse == n);
+  }
+
+  public static boolean perfect(int n) {
+    int num = 0;
+    for (int i = 1; i < n; i++) {
+      if (n % i == 0) {
+        num = num + i;
+      }
+
+    }
+    return (num == n);
+  }
+
+  public static int fact(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+      fact = fact * i;
+    }
+    return fact;
+  }
+
+  public static boolean strong(int n) {
+    int num = n;
+    int digit = 0;
+    int sum = 0;
+    while (n != 0) {
+      digit = n % 10;
+      sum = sum + fact(digit);
+      n = n / 10;
+    }
+    return (sum == num);
+  }
+
+}
